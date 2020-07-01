26a4a323d47eccc81524cd175dde5831a74195b9
miles zarathustra <mlzarathustra@gmail.com>
2020-07-01 13:51:44 -0700

    second take on the smoother seems to work.

--- src/main/java/ondes/synth/wire/Smooth.java
+++ src/main/java/ondes/synth/wire/Smooth.java
@@ -12,17 +12,12 @@ import java.util.Arrays;
 import java.util.Map;
 
 /**
- *   Attempt to smooth waves by limiting the delta
- *   in angle of the line.
- *
- *   Sounds like junk, but draws some pretty curves
- *   sometimes.
- *
+ *    smoothing similar to that used for the envelopes.
  */
 public class Smooth extends MonoComponent {
 
-    double y0, y1, y2;
-    double wLimit;
+    double y0;
+    float levelScale = 1f;
 
     @Override
     public void pause() {
@@ -30,49 +25,18 @@ public class Smooth extends MonoComponent {
 
     @Override
     public void resume() {
-        y0 = y1 = y2 = 0;
+        y0 = 0;
     }
 
     @Override
     public int currentValue() {
         int inp = inputSum();
-        //if (wLimit == 0) return inp;
         return currentValue(inp);
     }
 
-    private int currentValue1(int inp) {
-        double w1 = atan(y1 - y2);
-        double wInp = atan(inp - y1) - w1;
-        if (abs(wInp) > wLimit) {
-            if (inp == y1) {
-                y0 = y1 + tan(w1 - signum(w1) * wLimit);
-            }
-            else {
-                y0 = y1 + tan(w1 + (wLimit * signum(inp - y1)));
-            }
-        }
-        else y0 = inp;
-
-        out.print(String.format("inp=%4d  w1=%8.4f  wInp=%8.4f  wLimit=%8.4f  ", inp, w1, wInp, wLimit));
-        out.println(String.format("     y0=%8.4f y1=%8.4f y2=%8.4f", y0, y1, y2));
-
-        y2 = y1;
-        y1 = y0;
-
-        return (int) y0;
-    }
 
     // // // //
 
-
-//    double rate = .001; // in ms
-//    double d = ( rate * 44100.0 / 1000.0) / 4.616;
-//    double k = 1.0/d;
-//    double m = k;
-//
-//    void showDKM() {
-//        err.println("d="+d+"  k="+k+"  m="+m);
-//    }
 
     double k=.01;
 
@@ -81,7 +45,7 @@ public class Smooth extends MonoComponent {
         y0 = y0 + (signum(delta)*k + delta*k);
         if ((y0 < inp && delta<0) || (y0>inp && delta>0)) y0 = inp;
 
-        return (int)y0;
+        return (int)(levelScale * y0);
     }
 
 
@@ -91,11 +55,25 @@ public class Smooth extends MonoComponent {
     public void configure(Map config, Map components) {
         super.configure(config, components);
 
-        Double dblInp = getDouble(config.get("k"),
-            "Smooth: 'limit' must be a decimal number.");
+        Double dblInp = getDouble(config.get("amount"),
+            "Smooth: 'k' must be a decimal number.");
 
-        if (dblInp != null) k = abs(dblInp);
-        err.println("k is "+k);
+        if (dblInp != null) k = 1.0 / abs(dblInp);
+        //err.println("k is "+k);
+
+        String levelScaleErr =
+            "'level-scale' must be between 0 and 11. (floating) " +
+                "Yes, it goes to 11! \n" +
+                "(but you probably want it much lower than that)";
+        Float fltInp = getFloat(config.get("level-scale"), levelScaleErr);
+        if (fltInp != null) {
+            if (fltInp < 0 || fltInp >11) err.println(levelScaleErr);
+            else
+                levelScale = fltInp;
+        }
+
+
+
     }
 
 
@@ -104,7 +82,6 @@ public class Smooth extends MonoComponent {
 
     public static void main(String[] args) {
         Smooth smooth = new Smooth();
-        smooth.wLimit = PI / 20.0;
 
         int[] input = new int[50];
         int[] output = new int[50];
